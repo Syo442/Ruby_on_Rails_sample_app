9f43201522e3ec8115d3dce83e662d711bb6e742
Syo442 <syo0618.jm@gmail.com>
2020-06-03 16:25:54 +0000

    StaticPages 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pases/home'
+
+  get 'static_pases/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
